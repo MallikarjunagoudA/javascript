c96c16ccb32e5c39a1bb26667a03497d2693dc6b
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-06-10 20:59:42 +0530

    java script till arrays

--- js2022/Practice.js
+++ js2022/Practice.js
@@ -46,10 +46,105 @@ console.log(x===y) //false
 
 */
 
+//7.. For loop
+
+// for in loop
 
 
 
+// 8 .. function
 
+
+//sum is the example
+/*
+function sum(a, b) {
+    return a + b;
+}
+var c = sum(5, 8);
+console.log(c)
+
+//ananymous function
+
+var summ = function (a, b) {
+    return total = a + b;
+}
+
+var s = summ(5, 9);
+
+console.log(s);
+
+
+
+//arrow function
+var summ1 =() => {
+    return `sum is ${a+b}`;
+}
+
+var summ2 =() =>{
+    `sum is ${a+b}`
+}
+
+var summ3 =() => `sum is ${a+b}`
+*/
+// 10.. var and let const
+/*
+//var
+function sum(){
+    var va =5;
+    console.log("inner",va)
+    return va;
+}
+sum();
+
+console.log("innerToOuter",va) //va is declared inside the function sum. its not available outside the function.
+
+
+
+//let 
+function sum(){
+    let va2 =5;
+    console.log("va2 declared in fun scoped ",va2)
+    if(5==5){
+        let va =5;
+        console.log("va2 declared in fun scoped and inside one if block scoped ",va2)
+        console.log("va declared inside the if loop block scoped only and this is innner if block",va)
+    }
+    console.log("va declared inside the if loop block scoped only and this is outside the  if block",va)//error
+
+    return va;
+}
+
+sum();
+
+console.log("innerToOuter",va2)
+
+console.log("innerToOuter",va) //let 
+*/
+
+// `template literels`
+/*
+var myFirstName ="mallikarjuna";
+var myLastName ='Anandagoud';
+console.log(`here my details myfirstName ${myFirstName} and my last name is ${myLastName}`)
+*/
+
+
+// 11.. default parameters
+/*
+function mult(a,b){
+    return a*b;
+}
+
+console.log(mult(5)) //this will return NaN.
+
+
+function mult1(a,b=9){
+    return a*b;
+}
+
+console.log(mult1(5)) //45
+
+*/
 
 
 
