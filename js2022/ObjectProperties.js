e31955ce3795b5f005189aaf74ccc5071508bc69
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-06-21 11:22:35 +0530

    objects properties

---
+++ js2022/ObjectProperties.js
@@ -0,0 +1,24 @@
+// * Object properties
+
+// * we can now use dynamic properties
+
+let myName = "Mallikarjun"
+
+const myObj ={
+    [myName]: "hello dear bestie",
+    age: 26,
+    adress: "somanakatti"
+}
+
+// console.log(myObj);
+
+// * No need to write key and value, if both are same.
+
+ let name ="mallikarun"
+let age =25
+
+const emp ={
+    name,
+    age
+}
+ console.log(emp)
