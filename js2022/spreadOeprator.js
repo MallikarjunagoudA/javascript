54f251c05f99ea4103e07fbbfa8f189202ea04b4
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-06-21 12:19:22 +0530

    js spread operator

--- js2022/spreadOeprator.js
+++ js2022/spreadOeprator.js
@@ -9,3 +9,22 @@ let myFevColor =["red","black","green","blue","yellow"]
 let myFavourateColor =[...color,"blue","yellow"];
 
 console.log(myFavourateColor)
+
+
+//rest operator
+// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, providing a way to represent variadic functions in JavaScript.
+// it depends on when we will use the ... operator.
+function sum(...theArgs) {
+    let total = 0;
+    for (const arg of theArgs) {
+      total += arg;
+    }
+    return total;
+  }
+  
+  console.log(sum(1, 2, 3));
+  // expected output: 6
+  
+  console.log(sum(1, 2, 3, 4));
+  // expected output: 10
+  
