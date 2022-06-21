efe84d4ac5651a2e9ecef28d4cdae6aaccee8078
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-06-21 12:06:22 +0530

    js spread operator

---
+++ js2022/spreadOeprator.js
@@ -0,0 +1,11 @@
+// * spread operator
+
+let color = ["red","black","green"];
+
+//without spread
+let myFevColor =["red","black","green","blue","yellow"]
+
+//with spread
+let myFavourateColor =[...color,"blue","yellow"];
+
+console.log(myFavourateColor)
