375cfcc90bd56942d94133660902126cb510768a
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-12-04 02:39:44 +0530

    js functions

--- Vanilla_js_new_series/jsFunction/jsFunction.js
+++ Vanilla_js_new_series/jsFunction/jsFunction.js
@@ -27,7 +27,7 @@ calculate(5, 6,"+");
 
 
 //**function expression */
-function calculate (a, b, operator){
+function calculate1 (a, b, operator){
     let op =operator;
     let c =0;
     switch(op){
@@ -49,8 +49,12 @@ function calculate (a, b, operator){
 calculate(5, 6,"+");
 
 
-//**ES6 function expression */
- const calculate = (a, b, operator) => {
+//**ES6 arrow function expression */
+/*
+The arrow function this will take the outer scope 
+arrow function doesn't have the its own this. and we will not use new to invoke the arrow function.
+*/
+ const calculate2 = (a, b, operator) => {
     let op =operator;
     let c =0;
     switch(op){
@@ -70,5 +74,29 @@ calculate(5, 6,"+");
 }
 
 calculate(5, 6,"+");
+
+
+
+//** constructor function */
+/*
+Here we need to invoke a function with the //? "new" keyword
+we need to instantiate the method using the //? "this" keyword
+The global this is different from the local this or which is used inside the function. the node will use the global this and browser will use the window this.
+
+*/
+
+function shape(length, width){
+    this.length = length;
+    this.width = width;
+
+   this.draw = function(){
+        console.log(`drawing the shape `+ this.length + ' '+this.width);
+    }
+}
+
+var square = new shape(5, 5);
+square.draw();
+var rectangle  = new shape(2,4);
+
 
 
